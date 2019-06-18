7a2109094ee4551345f4683be13fa5997d7fae42
nippun99 <37634751+nippun99@users.noreply.github.com>
2019-06-18 14:35:11 +0530

    spiral print

--- 2d-Arrays/SpiralPrint.cpp
+++ 2d-Arrays/SpiralPrint.cpp
@@ -3,9 +3,30 @@
 using namespace std;
 
 void spiral(int a[][20],int r,int c){
+	int sr=0;
+	int er=r-1;
+	int sc=0;
+	int ec=c-1;
+	//print sr
 	
-	while(sr)
-	
+	while(sc<=ec && sr<=er){
+		for(int p=sc;p<=ec;p++){
+			cout<<a[sr][p]<<" ";
+		}
+		sr++;
+		for(int p=sr;p<=er;p++){
+			cout<<a[p][ec]<<" ";
+		}
+		ec--;
+		for(int p=ec;p>=sc;p--){
+			cout<<a[er][p]<<" ";
+		}		
+		er--;
+		for(int p=er;p>=sr;p--){
+			cout<<a[p][sc]<<" ";
+		}
+		sc++;
+}
 	
 	
 }
@@ -23,8 +44,16 @@ int main(){
 		}
 	}
 	
+	for(int i=0;i<row;i++){
+		for(int j=0;j<col;j++){
+			cout<<a[i][j]<<"  ";
+		}
+		cout<<endl;
+	}
 	
 	
+	
+	spiral(a,row,col);
 	
 
 }
